11b29526b80dadd06f0d8ddf22ae09a9c65d5531
marllanesgarcia <115183040+marllanesgarcia@users.noreply.github.com>
2023-05-31 11:21:13 +0200

    Update PantallaPregunta1.java

--- proyecto.final.DAW/src/main/java/interfaces/PantallaPregunta1.java
+++ proyecto.final.DAW/src/main/java/interfaces/PantallaPregunta1.java
@@ -47,7 +47,7 @@ public class PantallaPregunta1 extends JPanel{
 		setBackground(new Color(216, 191, 216));
 
 		this.ventana=v;
-		this.ventana.setSize(818,524);
+		this.ventana.setSize(820,530);
 		setLayout(null);
 		
 		JPanel panel = new JPanel();
